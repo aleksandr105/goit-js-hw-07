9a89ef258c5062adabd8991468dafacae3dda0d8
aleksandr105 <aleksandrsherbina105@gmail.com>
2021-12-09 21:40:31 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function onGalleryItems(items) {
+  return items
+    .map(
+      ({ original, preview, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+galleryEl.innerHTML = onGalleryItems(galleryItems);
